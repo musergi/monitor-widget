4876bbdb7c14fe413ee9741800603ada653c2347
musergi <sergi.mus@gmail.com>
2021-11-10 18:42:57 +0100

    Added setting parsing to widget

--- src/mtk_settings.c
+++ src/mtk_settings.c
@@ -1,5 +1,7 @@
 #include "mtk_settings.h"
 
+#include <gio/gio.h>
+
 #define PI 3.14159264
 
 void mtkSettingsLoadDefault(MtkSettings *settings) {
@@ -16,4 +18,9 @@ void mtkSettingsLoadDefault(MtkSettings *settings) {
   settings->centerFontSize = 40.0;
   settings->percentageFontSize = 30.0;
   settings->textSpacing = 8.0;
+
+  GSettings *gSettings = g_settings_new("org.gnome.shell.extensions.monitor");
+  settings->baseColor.r = g_settings_get_double(gSettings, "base-color-r");
+  settings->baseColor.g = g_settings_get_double(gSettings, "base-color-g");
+  settings->baseColor.b = g_settings_get_double(gSettings, "base-color-b");
 }
